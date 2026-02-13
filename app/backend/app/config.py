9025061112ea3932c96a9893145349f23ccc72ae
politifan <mihailpavlov042006@gmail.com>
2026-02-13 19:42:01 +0300

    env fix

--- app/backend/app/config.py
+++ app/backend/app/config.py
@@ -6,8 +6,8 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 @dataclass(frozen=True)

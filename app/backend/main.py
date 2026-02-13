f2c6e0f13ab484bb92c8cdb0b8999c44749b301d
politifan <mihailpavlov042006@gmail.com>
2026-02-13 19:35:36 +0300

    fix

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,3 +1,8 @@
-from app.application import app
+try:
+    # Import path when module is loaded as package: app.backend.main
+    from .app.application import app
+except ImportError:
+    # Import path when running from app/backend directory: uvicorn main:app
+    from app.application import app
 
 __all__ = ["app"]

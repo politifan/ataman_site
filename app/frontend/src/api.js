c01de7a43b0940b8afd08e86d7711588a4a0f5e3
politifan <mihailpavlov042006@gmail.com>
2026-02-20 17:09:16 +0300

    баг фикс

--- app/frontend/src/api.js
+++ app/frontend/src/api.js
@@ -5,11 +5,11 @@ const ADMIN_TOKEN_STORAGE_KEY = "atman_admin_token";
 
 async function request(path, options = {}) {
   const response = await fetch(`${API_BASE}${path}`, {
+    ...options,
     headers: {
       "Content-Type": "application/json",
       ...(options.headers || {})
-    },
-    ...options
+    }
   });
 
   if (!response.ok) {
